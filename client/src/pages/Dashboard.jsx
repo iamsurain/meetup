2c6345e0019697835d6373ae7a2950bf94247879
iamsurain <surain5262@gmail.com>
2026-08-28 17:17:35 +0530

    created meeting room UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -43,11 +43,11 @@ const Dashboard = () => {
     e.preventDefault();
     const cleanId = joinId.trim();
 
-    if (!cleanId) {
+    if (!/^[a-z]{3}(?:-[a-z]{3}){2}$/.test(cleanId)) {
         toast.error("Please enter a valid Meeting ID");
         return;
     }
-    navigate(`/meeting/${cleanId}`)
+    navigate(`/meeting/${encodeURIComponent(cleanId)}`)
   }
 
   return (
